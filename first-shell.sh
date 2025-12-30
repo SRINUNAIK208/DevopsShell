8bb29e57a1ed92b075a47c7ae9914a095e803eba
SRINUNAIK208 <srinaik208@gmail>
2025-12-30 18:57:53 +0530

    sql

--- first-shell.sh
+++ first-shell.sh
@@ -7,6 +7,12 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
+Logs_Folder="/var/log/shellscript-log"
+Script_Name="$(echo $0 | cut -d "." -f1)"
+Logs_file="$Logs_Folder/$Script_Name.log"
+
+mkdir -p Logs_Folder
+
 UserId=$(id -u)
 
 if [ $UserId -eq 0 ]
@@ -31,13 +37,36 @@ VALIDATE(){
 
 }
 
-dnf list installed mysql
+dnf list installed mysql &>> $Logs_file
 if [ $? -eq 0 ]
 then
     echo -e "$Y mysql is already installed...Nothing to do $N"
 else
     echo -e "$R mysql is not installed..going to installing $N"
-    dnf install mysql -y
+    dnf install mysql -y &>> &Logs_file
     VALIDATE $? "mysql"
+fi
+
+dnf list installed nginx &>> $Logs_file
+
+if [ $? -eq 0 ]
+then 
+   echo "$Y nginx is already installed..Nothing to do $N"
+else
+   echo "$R nginx is not installed..going to installing $N"
+   dnf install nginx -y &>> $Logs_file
+   VALIDATE $? "nginx"
+   exit 1
+fi
+dnf list installed python3 &>> $Logs_file
+
+if [ $? -eq 0 ]
+then 
+   echo "$Y python3 is already installed..Nothing to do $N"
+else
+   echo "$R python3 is not installed..going ti installing $N"
+   dnf install python3 -y &>> $Logs_file
+   VALIDATE $? "python3"
+   exit 1
 fi
     
